897c1c2cdb62e3ead5e4b2305b75489c75d95e81
gmllr <43215443+gmllr@users.noreply.github.com>
2023-01-29 14:52:15 -0500

    add footer component

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Footer.module.css';
+import Link from 'next/link';
+
+export default function Footer() {
+    return (
+        <>
+            <div className={styles.footer}>
+                <div>
+                    This is the footer
+                </div>
+            </div>
+        </>
+
+
+    )
+}
