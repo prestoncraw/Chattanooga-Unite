695fe369bb9f36d3a7c6f6a71dcc2934e4f4000d
gmllr <43215443+gmllr@users.noreply.github.com>
2023-01-29 14:56:33 -0500

    minor ui changes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          <a href="https://setnvets.org">Veterans Resource Center - Chattanooga Unite</a>
+          Veterans Resource Center - Chattanooga Unite
         </h1>
         <p>
           Providing outreach to all military service persons including veterans and their familes by creating partnerships between VA providers and local agencies.
