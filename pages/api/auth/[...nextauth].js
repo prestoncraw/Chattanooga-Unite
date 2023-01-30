4ca57903e562a0a8b9eaedeee0f4103c2fb8c6c1
gmllr <43215443+gmllr@users.noreply.github.com>
2023-01-30 17:53:36 -0500

    fix auth0 not invalidating session when logout button is clicked

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,11 @@ export const authOptions = {
             clientId: process.env.AUTH0_CLIENT_ID,
             clientSecret: process.env.AUTH0_CLIENT_SECRET,
             issuer: process.env.AUTH0_ISSUER,
+            authorization: {
+                params: {
+                    prompt: "login",
+                },
+            },
         })
     ],
 }
