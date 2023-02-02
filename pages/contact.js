4e1b75f2da1361c9c7fb48c97541f23360233888
prestoncraw <prestoncrawford530@gmail.com>
2023-02-02 17:54:49 -0500

    small changes to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -12,15 +12,15 @@ function Contact() {
             </Head>
             <main>
             <NavBar/>
-                <div className={styles.grayBackground}>
+                <div className={styles.grayBackgroundContact}>
                     <div className={styles.blue_decoration}></div>
                     <div className={styles.title_dark}>Contact Us</div><br></br>
                     <div className={styles.description_dark}>
-                        <p>1 Siskin Plaza, Chattanooga, TN 37403</p>
-                        <p>(423) 634-1771</p>
-                        <p>infosetnvets@gmail.com</p>
-                        <p>Please call or e-mail for information about the Coalition and Coalition Partnership, or to request electronic copies of the Southeast Tennessee Veterans Coalition brochure.</p>
-                        <p>All other inquiries, please dial 211.</p>
+                    <div className={styles.bold}><p>1 Siskin Plaza, Chattanooga, TN 37403</p></div>    
+                    <div className={styles.bold}><p>(423) 634-1771</p></div>    
+                    <div className={styles.bold}><p>infosetnvets@gmail.com</p></div>
+                    <div className={styles.bold}><p>Please call or e-mail for information about the Coalition and Coalition Partnership, or to request electronic copies of the Southeast Tennessee Veterans Coalition brochure.</p></div>
+                    <div className={styles.bold}><p>All other inquiries, please dial 211.</p></div>
                     </div>
                 </div>
             </main>
