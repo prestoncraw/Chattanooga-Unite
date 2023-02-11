d00a2e13326f48e83dc82c955f6877b41d857fd9
ArialEC <arialcofer01@gmail.com>
2023-02-10 19:26:34 -0500

    updated home page & css for pics & page max-width

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Home &raquo; Chattanooga Unite - Veterans Resource Center</title>
         <link rel="icon" href="/favicon.ico" />
@@ -17,21 +17,30 @@ export default function Home() {
       <main>
       <NavBar/>
         <div className="grayBackground centered">
+          <div className={styles.titleBackground}>
+            <Image
+              src="/images/handshake.png"
+              alt="Picture of Veteran with tiny house in the palm of his hand"
+              width={300}
+              height={300}
+            />
+          </div>
             <h1 className="title">Veterans Resource Center<br></br>Chattanooga Unite <span className={styles.red}><i className="fa-solid fa-star"></i></span></h1>
-            <p className={styles["corners"] + " " + styles["description_dark"]}>
+            <p className="corners description_dark">
               Providing outreach to all military service persons including veterans and their familes by creating partnerships between VA providers and local agencies.
             </p>
-            <Link href='/find-help'><button className={styles.btn_outline_dark}>Find Help <i className="fa-solid fa-arrow-right"></i></button></Link>
+            <Link href='/find-help'><button className="btn_outline_dark">Find Help <i className="fa-solid fa-arrow-right"></i></button></Link>
+              <p className="description_dark">Questions? Call 2-1-1 United Way of Greater Chattanooga</p>
         </div>
 
-        <div className={styles.blueBackground}>
-          <div className={styles.left}>
-            <div className={styles.white_decoration}></div>
-            <div className={styles.title_light}>Helping Veterans and Their Families</div><br></br>
-            <p className={styles.description_light}>This effort grew out of the Iraq and Afghanistan wars with a desire to partner with the Veterans Administration in providing services for the increasing challenges of these returning veterans, while satisfying current needs of existing veterans and their families.</p><br></br>
-            <p className={styles.description_light}>Epilepsy Foundation of Southeast Tennessee (EFSETN) along with over 100+ other agencies and businesses are a part of this community grassroots effort. This grassroots coalition of agencies is called the Southeast Tennessee Veterans Coalition. Presently, EFSETN chairs the Coalition.</p>
+        <div className="blueBackground">
+          <div className="left">
+            <div className="white_decoration"></div>
+            <div className="title_light">Helping Veterans and Their Families</div><br></br>
+            <p className="description_light">This effort grew out of the Iraq and Afghanistan wars with a desire to partner with the Veterans Administration in providing services for the increasing challenges of these returning veterans, while satisfying current needs of existing veterans and their families.</p><br></br>
+            <p className="description_light">Epilepsy Foundation of Southeast Tennessee (EFSETN) along with over 100+ other agencies and businesses are a part of this community grassroots effort. This grassroots coalition of agencies is called the Southeast Tennessee Veterans Coalition. Presently, EFSETN chairs the Coalition.</p>
           </div>
-          <div className={styles.right}>
+          <div className="right">
             <Image
                 src="/images/landing page photo1.png"
                 alt="Picture of Veteran with tiny house in the palm of his hand"
@@ -41,15 +50,18 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.grayBackground}>
-          <div className={styles.right}>
-            <div className={styles.blue_decoration}></div>
-            <h1 className={styles.title_dark}>Proudly Serving All Military</h1>
-            <p className={styles.description_dark}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et aliquet tellus. Vestibulum laoreet est at dui porttitor pulvinar. Curabitur sit amet massa vehicula neque sagittis placerat. Vestibulum purus libero, aliquam quis interdum non, malesuada lacinia nunc. Proin lorem justo, mollis sit amet nunc at, lacinia semper lorem. Duis tellus erat, gravida et aliquam vitae, rhoncus sed justo. Nulla in ultrices sem. Curabitur imperdiet, libero blandit pulvinar congue, dui ipsum dictum purus, sed pulvinar arcu sapien ut risus. Vivamus sed lorem elit. Maecenas eu nibh ut lectus blandit maximus ut eleifend massa. Duis in bibendum metus. Nullam pulvinar vulputate dui, id maximus enim viverra nec. Fusce ac metus eros. Curabitur nec lacus consequat, cursus eros id, commodo quam. Sed sed quam tortor. Nulla non mauris et tellus dictum sagittis.
+        <div className="grayBackground">
+          <div className="right">
+            <div className="blue_decoration"></div>
+            <h1 className="title_dark">Proudly Serving All Military</h1>
+            <p className="description_dark">
+              We are proud to have partnerships with over 150 organizations in South East Tennessee and its surrounding areas. 
+              We connect veterans with the resources they need in over 15 counties accross Tennessee, Georgia, and Alabama.
+              We are able to help provide veterans with access to 13 different types of assistance. <br></br><br></br>
+              Questions? Call 2-1-1 United Way of Greater Chattanooga. 
             </p>
           </div>
-          <div className={styles.left}>
+          <div className="left">
             <Image
                 src="/images/landing page photo2.png"
                 alt="Picture of smiling Veteran in wheelchair"
