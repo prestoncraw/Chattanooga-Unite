24c76a54994419c01073ab125dcc904a476a9007
gmllr <43215443+gmllr@users.noreply.github.com>
2023-02-20 20:59:39 -0500

    add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,32 @@
+import Head from "next/head"
+import NavBar from "../components/navbar"
+
+export default function Custom404() {
+    return (
+        <>
+            <Head>
+                <title>Page Not Found &raquo; Chattanooga Unite - Veterans Resource Center</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Chattanooga Unite provides outreach to all military service persons including veterans and their familes by creating partnerships between VA providers and local agencies for those in the Chattanooga area."
+                    key="desc"
+                />
+                <meta property="og:title" content="Page Not Found / Chattanooga Unite - Veterans Resource Center" />
+                <meta
+                    property="og:description"
+                    content="Chattanooga Unite provides outreach to all military service persons including veterans and their familes by creating partnerships between VA providers and local agencies for those in the Chattanooga area."
+                />
+                <meta
+                    property="og:image"
+                    content="https://pub-62dc534c19094cf6b30ce047dde383e3.r2.dev/unite.jpg"
+                />
+            </Head>
+            <div>
+                <NavBar/>
+                <h1>Page Not Found!</h1>
+            </div>
+            
+        </>
+    )
+}
