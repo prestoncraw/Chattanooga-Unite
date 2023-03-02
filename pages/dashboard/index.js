cf23228c40b0a36a05e2c711df3327b6780be5d6
gmllr <43215443+gmllr@users.noreply.github.com>
2023-03-02 13:59:33 -0500

    add org pages to dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,13 @@
 import { useSession, getSession, signOut } from "next-auth/react"
+import { useState, useEffect } from "react";
+// import { authOptions } from 'pages/api/auth/[...nextauth]'
+import { getServerSession } from "next-auth/next"
+import SPPreview from "../../components/sp-preview";
 
-export default function Dashboard() {
-    const { data: session, status } = useSession()
+export default function Dashboard({data}) {
+    const { data: session, status } = useSession();
+
+    const [userData, setUserData] = useState(JSON.parse(data.data)[0])
 
     if (status === "loading") {
         return <p>Loading...</p>
@@ -13,7 +19,32 @@ export default function Dashboard() {
     return (
         <>
             <div>You are logged in as {session.user.email}</div>
+            <a href={"/dashboard/org/"+ userData.id}>Edit your Organization</a>
+            {/* <SPPreview key={userData.id} provider={userData} /> */}
             <button onClick={() => signOut({ callbackUrl: '/' })}>Sign out</button>
         </>
     )
+}
+
+export async function getServerSideProps(context) {
+    const session = await getServerSession(context.req, context.res);
+    console.log(session.user.email);
+    const res = await fetch(`http://localhost:3000/api/get-sp?sp_email=${session.user.email}`);
+    const data = await res.json();
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {
+            session,
+            data
+        },
+    }
 }
