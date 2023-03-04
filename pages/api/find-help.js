1fc8e3b4c61bf0f17e22c9e78ccb797e895f2365
prestoncraw <prestoncrawford530@gmail.com>
2023-03-03 19:45:33 -0500

    implement metric tracking v1

--- pages/api/find-help.js
+++ pages/api/find-help.js
@@ -1,17 +1,29 @@
 import executeQuery from '../../lib/db';
+import moment from 'moment';
 
 export default async function handler(req, res) {
     const { service_id, county_id } = req.query;
+    const search_timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
 
-    const query = 
-    `SELECT sp.* FROM service_providers sp JOIN sp_services s ON sp.id = s.service_provider_id 
-    JOIN sp_counties c ON sp.id = c.service_provider_id 
-    JOIN service se ON s.service_id = se.id JOIN county co ON c.county_id = co.id 
-    WHERE se.id = ? AND co.id = ?`;
+    const selectQuery = `
+      SELECT sp.* 
+      FROM service_providers sp 
+      JOIN sp_services s ON sp.id = s.service_provider_id 
+      JOIN sp_counties c ON sp.id = c.service_provider_id 
+      JOIN service se ON s.service_id = se.id 
+      JOIN county co ON c.county_id = co.id 
+      WHERE se.id = ? AND co.id = ?
+    `;
+    const selectValues = [service_id, county_id];
+    const serviceProviders = await executeQuery({query: selectQuery, values: selectValues});
 
-    const values = [service_id, county_id];
-    const serviceProviders = await executeQuery({query, values});
-    // console.log(serviceProviders);
+    const foundMatch = serviceProviders.length > 0 ? 1 : 0;
+    const insertQuery = `
+      INSERT INTO sp_search_metrics (search_timestamp, county_id, service_id, found_match) 
+      VALUES (?, ?, ?, ?)
+    `;
+    const insertValues = [search_timestamp, county_id, service_id, foundMatch];
+    const insertData = await executeQuery({query: insertQuery, values: insertValues});
 
-    res.status(200).json({ data: serviceProviders }) 
+    res.status(200).json({ data: serviceProviders });
 }
