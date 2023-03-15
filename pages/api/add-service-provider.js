7e5eee1edb9eda2acbfd768c348ec199952c0595
Christianboyd7 <SXB176@mocs.utc.edu>
2023-03-15 18:27:39 -0400

    updated add service provider function

--- pages/api/add-service-provider.js
+++ pages/api/add-service-provider.js
@@ -2,9 +2,21 @@ import executeQuery from '../../lib/db';
 
 export default async function handler(req, res) {
   try {
-    const query = `INSERT INTO service_providers (logo_url, name, description, contact_phone_number, contact_email, website_url, address, login_email) VALUES (?,?,?,?,?,?,?,?)`;
-    const serviceProviders = await executeQuery({ query });
-    res.json(serviceProviders).status(200);
+    
+    const { name, email } = req.query;
+
+    const insert_user_query = `INSERT IGNORE INTO users (email, is_admin) VALUES (?,0)`;
+
+    const insert_sp_query = `INSERT INTO service_providers (name, login_email) VALUES (?,?)`;
+
+    const userValues = [email];
+    const add_user = await executeQuery({query: insert_user_query, values: userValues});
+
+    const spValues = [name, email];
+    const serviceProviders = await executeQuery({query: insert_sp_query, values: spValues});
+
+    res.send("Successfully added service provider").status(200);
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
