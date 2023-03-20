06c5c1b56b5d5612e52a275bb322472e54318369
gmllr <43215443+gmllr@users.noreply.github.com>
2023-03-20 06:11:47 -0400

    create basic admin-options and org-panel components for dashboard homescreen

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -10,8 +10,10 @@ import DashboardComp from '../../components/dashboard/dashboard';
 import { Typography } from "@mui/material";
 
 import getAuthUser from "../../lib/get-auth-user";
+import AdminOptionsPanel from "../../components/dashboard/admin-options-panel";
+import OrgPanel from "../../components/dashboard/org-panel";
 
-export default function Dashboard({user, session}) {
+export default function Dashboard({ user, session }) {
     const { status } = useSession();
 
 
@@ -26,25 +28,30 @@ export default function Dashboard({user, session}) {
     }
     return (
         <>
-          <Head>
-            <title>Dashboard &raquo; Chattanooga Unite - Veterans Resource Center</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <Navbar/>
-          <Typography variant="h2" component="div" style={{ mt: 8 }}>
-            Dashboard Menu
-          </Typography>
-          {/* <DashboardComp/> */}
-          <div>You are logged in as {userData.user_email}</div>
-          <div>is_admin: {userData.is_admin}</div>
-          {/* <a href={"/dashboard/org/"+ userData.id}>Edit your Organization</a> */}
+            <Head>
+                <title>Dashboard &raquo; Chattanooga Unite - Veterans Resource Center</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Navbar />
+
+            <Typography variant="h2" component="div" style={{ mt: 8 }}>
+                Dashboard Menu
+            </Typography>
+
+
+            {/* <DashboardComp/> */}
+            {/* <div>You are logged in as {userData.user_email}</div> */}
+
+            {userData.is_admin==true && <AdminOptionsPanel />}
+            <OrgPanel organizations={userData.Organizations}></OrgPanel>
         </>
-      );
+    );
 }
 
 export async function getServerSideProps(context) {
     const session = await getServerSession(context.req, context.res);
-    
+
     if (!session) {
         return {
             redirect: {
@@ -53,14 +60,7 @@ export async function getServerSideProps(context) {
             },
         }
     }
-
-    // console.log(session.user.email);
-    
-    // const res = await fetch(`http://localhost:3000/api/get-sp?sp_email=${session.user.email}`);
-    // const data = await res.json();
-
     const user = await getAuthUser(session.user);
-    // const user = data;
 
     return {
         props: {
