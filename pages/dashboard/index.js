d65a3b7a4ba84cc0bb7ee2e4f3ce7f1aaa6d553e
gmllr <43215443+gmllr@users.noreply.github.com>
2023-03-20 05:32:33 -0400

    create function to get context for logged in user

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -9,11 +9,13 @@ import Navbar from '../../components/dashboard/navbar';
 import DashboardComp from '../../components/dashboard/dashboard';
 import { Typography } from "@mui/material";
 
-export default function Dashboard({data}) {
-    const { data: session, status } = useSession();
+import getAuthUser from "../../lib/get-auth-user";
+
+export default function Dashboard({user, session}) {
+    const { status } = useSession();
 
 
-    const [userData, setUserData] = useState(JSON.parse(data.data)[0])
+    const [userData] = useState(user);
 
     if (status === "loading") {
         return <p>Loading...</p>
@@ -29,22 +31,20 @@ export default function Dashboard({data}) {
             <link rel="icon" href="/favicon.ico" />
           </Head>
           <Navbar/>
-          <Typography variant="h3" component="div" style={{ mt: 8 }}>
-            Metric Menu
+          <Typography variant="h2" component="div" style={{ mt: 8 }}>
+            Dashboard Menu
           </Typography>
-          <DashboardComp/>
-          <div>You are logged in as {session.user.email}</div>
-          <a href={"/dashboard/org/"+ userData.id}>Edit your Organization</a>
+          {/* <DashboardComp/> */}
+          <div>You are logged in as {userData.user_email}</div>
+          <div>is_admin: {userData.is_admin}</div>
+          {/* <a href={"/dashboard/org/"+ userData.id}>Edit your Organization</a> */}
         </>
       );
 }
 
 export async function getServerSideProps(context) {
     const session = await getServerSession(context.req, context.res);
-    console.log(session.user.email);
-    const res = await fetch(`http://localhost:3000/api/get-sp?sp_email=${session.user.email}`);
-    const data = await res.json();
-
+    
     if (!session) {
         return {
             redirect: {
@@ -54,9 +54,18 @@ export async function getServerSideProps(context) {
         }
     }
 
+    // console.log(session.user.email);
+    
+    // const res = await fetch(`http://localhost:3000/api/get-sp?sp_email=${session.user.email}`);
+    // const data = await res.json();
+
+    const user = await getAuthUser(session.user);
+    // const user = data;
+
     return {
         props: {
-            data
+            user,
+            session
         },
     }
 }
