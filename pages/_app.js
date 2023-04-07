223a4cb9cad3f88ad6258fb109181024aaa1ebd2
prestoncraw <prestoncrawford530@gmail.com>
2023-04-06 20:43:26 -0400

    conditionally render AccessibilityMenu

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,19 @@ import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
 import Script from "next/script";
 import AccessibilityMenu from "../components/accessibiliy-menu";
+import { useRouter } from 'next/router';
 
 export default function App({
     Component,
     pageProps: { session, ...pageProps },
 }) {
+    const router = useRouter();
+    const isDashboard = router.pathname.startsWith('/dashboard');
+
     return (
         <SessionProvider session={session}>
             <Script src="https://kit.fontawesome.com/8e69a0977a.js" crossOrigin="anonymous" />
-            <AccessibilityMenu />
+            {!isDashboard && <AccessibilityMenu />}
             <Component {...pageProps} />
         </SessionProvider>
     )
