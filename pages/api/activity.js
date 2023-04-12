3fab1139790a2648657210422f64d374b3b2096d
gmllr <43215443+gmllr@users.noreply.github.com>
2023-04-12 00:22:33 -0400

    create standard function for authorizing requests

--- pages/api/activity.js
+++ pages/api/activity.js
@@ -1,28 +1,16 @@
-import { access } from 'fs';
 import executeQuery from '../../lib/db';
-import getAuthUser from '../../lib/get-auth-user';
-import authorizeUser from '../../lib/authorize-user';
-import { getServerSession } from "next-auth/next";
+import authorizeRequest from '../../lib/authorize-request';
 
 export default async function handler(req, res) {
 
-  const session = await getServerSession(req, res)
-    if (!session) {
-      res.status(401).send("Access Denied")
-    }
+  if (!(await authorizeRequest(req, res, "admin"))) {
+    res.status(401).send("Access Denied");
+  }
 
-    const user = await getAuthUser(session.user);
-    const isAuthorized = await authorizeUser(user, 0);
-
-    if (isAuthorized == false) {
-      res.status(401).send("Access Denied")
-    }
-
-  const query = `SELECT a.*, u.* FROM activity_log a JOIN users u ON a.user_id = u.id`;
-  
-  if(isAuthorized == true){
+  else {
+    const query = `SELECT a.*, u.* FROM activity_log a JOIN users u ON a.user_id = u.id`;
     const log = await executeQuery({ query });
-  const activityLog = JSON.parse(log)
-  res.json(activityLog).status(200);
+    const activityLog = JSON.parse(log)
+    res.status(200).json(activityLog);
   }
 }
